9ab31af900c9c1155b4f7d3f010b130333a57c12
Nguyen Dang Nhat <dangnhatnguyen@outlook.com>
2021-11-13 19:08:19 +0700

    ver 1

--- lib/adaptive_scaffold.dart
+++ lib/adaptive_scaffold.dart
@@ -1,7 +1,4 @@
 library adaptive_scaffold;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/adaptive_scaffold.dart';
+export 'src/models.dart';
